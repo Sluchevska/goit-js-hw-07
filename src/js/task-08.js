fb200a3630cddad44a32a1a85e3a02d3479c88d6
Sluchevska <alaska3a@gmail.com>
2021-06-03 12:48:05 +0100

    update task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -10,31 +10,29 @@ const createBoxes = (amount) => {
     for (let i = 0; i < amount; i += 1){
         const boxes = document.createElement('div')
         boxes.classList.add('boxes')
-     
+             
         boxes.style.height = 30 + i * 10 + 'px'
-        boxes.style.width = 30 + i * 10 + 'px'        
-        boxes.style.backgroundColor = Math.floor(Math.random()*256)
+        boxes.style.width = 30 + i * 10 + 'px'
+        const color = Math.floor(Math.random() * 256)
+        const color1 = Math.floor(Math.random() * 256)
+        const color2 = Math.floor(Math.random() * 256)
+        boxes.style.backgroundColor = 'rgb('+color+','+color1+','+color2+')'
+      
+        
     
         boxHolder.appendChild(boxes)
     }
-            
-   
-    console.log(boxHolder)
    
 }
 
-
-
 const destroyBoxes = (amount) => {
     amount = inputHolder.value
-     console.log(amount)
+   
     for (let i = 1; i <= amount; i += 1)
     {
       
         boxHolder.removeChild(boxHolder.querySelector('.boxes'))
-    
-     
-    console.log(boxHolder)}
+    }
     
 }
 
